51ebc809d4350e0604ee1170e915789489517898
unknown <goosrobin1@gmail.com>
2020-11-10 16:15:06 +0100

    optimized

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ class SerialKmeans {
       * 
       * Pick k centroids by using random with specific seed from the data.
       */
-     vector<vector<double>> initializeCentroids(vector<vector<double>> &data, int k){
+     vector<vector<double>> initializeCentroids(const vector<vector<double>> &data, int k){
           vector<vector<double>> centroids;
           for(int i = 0; i < k; i++){
                centroids.push_back(data[0 + rand() % data.size()]);
@@ -62,7 +62,7 @@ class SerialKmeans {
       * this Function starts the Kmeans algorithm and writes the output / trace
       * if specified by the user.
       */
-     void kMeans(vector<vector<double>> &data, int k, int repetitions){
+     void kMeans(const vector<vector<double>> &data, int k, int repetitions){
           double cost;
           double lowest_cost = 1000000000;
           vector<double> clusters;
@@ -70,32 +70,36 @@ class SerialKmeans {
           vector<vector<double>> centroids;
           vector<vector<double>> upCentroids;
           Timer timer1;
+          Timer t;
           
           timer1.start();
           for(int i = 0; i < repetitions; i++){
                centroids.clear();
                clusters.clear();
+               int counter = 0;
 
+               
                centroids = initializeCentroids(data, k);
-               clusters = getClosestCentroid(data, centroids, k);    
+               clusters = getClosestCentroid(data, centroids, k);
 
                if(trace != "" && i == 0){
                     writeTraceCSV(clusters);
-               }
-                          
-               upCentroids = updateCentroids(data, clusters, k);
-               int counter = 0;
+               }       
 
+               upCentroids = updateCentroids(data, clusters, k);
                lowest_cost = costFunction(upCentroids, data, clusters, k);
                endCluster = clusters;
                
                while(upCentroids != centroids && counter < 500){
                     centroids = upCentroids;
                     upCentroids.clear();
+             
                     clusters = getClosestCentroid(data, centroids, k);
+
                     upCentroids = updateCentroids(data, clusters, k);
+
                     counter++;
-                    
+
                     if(trace != "" && i == 0){
                          writeTraceCSV(clusters);
                     }
@@ -118,7 +122,7 @@ class SerialKmeans {
       /**
       * Calculates the cost of all clusters per repetition.
       */
-     double costFunction(vector<vector<double>> upCentroids, vector<vector<double>> &data, vector<double> &clusters, int k){
+     double costFunction(vector<vector<double>> upCentroids,const vector<vector<double>> &data, vector<double> clusters, int k){
           double total = 0;
 
           for(int i = 0; i < data.size(); i++){  
@@ -128,7 +132,6 @@ class SerialKmeans {
                     }
                }
           }
-
           return total;
      }
      
@@ -155,39 +158,35 @@ class SerialKmeans {
       * 
       * This function calculates the lowest distance between the points and the chosen centroids.
       */
-     vector<double> getClosestCentroid(vector<vector<double>> &data, vector<vector<double>> &centroids, int k){
+     vector<double> getClosestCentroid(const vector<vector<double>> &data, vector<vector<double>> centroids, int k){
           double lowestDist, clusterOfLowestDistance;
           unsigned long data_size = data.size();
           unsigned long cluster_size = k;
           vector<double> clusters;
 
           #pragma omp parallel
-          {
+          {     
                vector<double> priv_clusters;
                #pragma omp for firstprivate(data_size, cluster_size) private(lowestDist, clusterOfLowestDistance) nowait schedule(static)
                for(int i = 0; i < data_size; i++){  
                     clusterOfLowestDistance = 0;
-                    lowestDist = 10000000;
-                    vector<double> &data_point = data[i];
-                    for(int j = 0; j < cluster_size; j++){   
-                         vector<double> &centroid = centroids[j];
-                         
-                         if(data_point != centroid){
-                              double currentDist = euclidDistance(data_point, centroid);
+                    lowestDist = 10000000000000000;
+                    for(int j = 0; j < cluster_size; j++){
+                         if(data[i] != centroids[j]){
+                              double currentDist = euclidDistance(data[i], centroids[j]);
                               if(currentDist <= lowestDist){
                                    lowestDist = currentDist;
                                    clusterOfLowestDistance = j;
-                              }  
+                              }
                          }
                     }
-                    priv_clusters.push_back(clusterOfLowestDistance);
+                    priv_clusters.emplace_back(clusterOfLowestDistance);
                }
                #pragma omp for schedule(static) ordered
                for(int i = 0; i<omp_get_num_threads(); i++){
                     #pragma omp ordered
                     clusters.insert(clusters.end(), priv_clusters.begin(), priv_clusters.end());
                }
-               
           }
           return clusters;
      }
@@ -198,8 +197,8 @@ class SerialKmeans {
      * inside each cluster.
      * @return a 2D vector with new Centroids.
      */
-     vector<vector<double>> updateCentroids(vector<vector<double>> &data, vector<double> clusters, int k){
-          double mean = 0, total = 0;
+     vector<vector<double>> updateCentroids(const vector<vector<double>> &data, vector<double> &clusters, int k){
+          double total = 0;
           int cluster_size = 0;
           int temp = 0;
           vector<double> punt;
@@ -207,32 +206,30 @@ class SerialKmeans {
 
           int dim_size = data[0].size();
           int n_size = data.size();
-
+          
+          
           for(int i = 0; i < k; i++){
                #pragma omp parallel
                {
                     vector<double> priv_punt;
-                    #pragma omp for firstprivate(dim_size, n_size) private(mean, total, cluster_size) nowait schedule(static)
+                    #pragma omp for firstprivate(dim_size, n_size) private(total, cluster_size) nowait schedule(auto)
                     for(int j = 0; j < dim_size; j++){ 
-                         total= 0;
-                         mean = 0;
+                         total = 0;
                          cluster_size = 0;
-                         for(int a = 0; a < n_size; a++){    
-                              double pt = data[a][j];   
+                         for(int a = 0; a < n_size; a++){      
                               if(clusters[a] == i){      
-                                   total += pt;
+                                   total += data[a][j];
                                    cluster_size++;
                               }    
                          } 
-
-                         mean = total / cluster_size;
-                         priv_punt.push_back(mean);
+                         total /= cluster_size;
+                         priv_punt.emplace_back(total);
                     }
                     #pragma omp for schedule(static) ordered
                     for(int i = 0; i<omp_get_num_threads(); i++){
                          #pragma omp ordered
                          punt.insert(punt.end(), priv_punt.begin(), priv_punt.end());
-                    }                   
+                    }                  
                }
                newCentroids.push_back(punt);
                punt.clear();
@@ -250,13 +247,10 @@ class SerialKmeans {
      */
      double euclidDistance(vector<double> punt, vector<double> centroid)
      {
-          double dist;
-
+          double dist = 0;
           for(int i = 0; i < punt.size(); i++){
-               dist += pow(punt[i] - centroid[i], 2);            
+               dist += (punt[i] - centroid[i])*(punt[i] - centroid[i]);            
           }  
-                       
-          dist = sqrt(dist); 
 	     return dist;
      }
 };
@@ -286,7 +280,7 @@ int main(int argc, char *argv[]){
      vector<vector<double>> data;
      string input_file, output_file = "", trace_file = "";
      int k, repetitions = 1, seed = 1850493;
-     omp_set_nested(1);
+     //omp_set_nested(1);
 
      for(int i = 0; i < argc; ++i){
           string arg = argv[i];
